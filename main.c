4a25062db0cd9f1c20125471a4dc9344d8ea0f8c
Ahmad Othman <32404077+Arthod@users.noreply.github.com>
2022-10-19 00:23:23 +0200

    enwik attempt

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include "compress.c"
 
-#define BLOCK_SIZE_MAX 600000000//2000000000//500000000 * 4
+#define BLOCK_SIZE_MAX 200000000//2000000000//500000000 * 4
                     // 2147483647
 
 #define min(a,b) (((a)<(b))?(a):(b))
@@ -40,18 +40,24 @@ int main(int argc, char** argv) {
     int blocks_count = ceil(f_size / BLOCK_SIZE_MAX);
     for (int i = 0; i < blocks_count; i++) {
         int block_size = min(BLOCK_SIZE_MAX, f_size - BLOCK_SIZE_MAX * i);
-        char *buffer = malloc(block_size * sizeof(char));
-        fgets(buffer, block_size + 1, f);
+        char *block = malloc(block_size * sizeof(char));
+        fgets(block, block_size + 1, f);
+
+        int runs = runs_count(block, block_size);
+        printf("Run length before %d\n", runs);
         printf("Compressing block %d of %d with block_size=%d\n", i + 1, blocks_count, block_size);
 
-        int64_t bwt_primary_index = bwt_transform(buffer, block_size);
-        printf("Run length %d\n", runs_count(buffer, block_size));
+        int64_t bwt_primary_index = bwt_transform(block, block_size);
+        runs = runs_count(block, block_size);
+        printf("Run length after %d\n", runs);
 
         // Write transformed to out
         FILE *f_out = fopen("out.txt", "w");
         for (int j = 0; j < block_size; j++) {
-            fprintf(f_out, "%c", buffer[j]);
+            fprintf(f_out, "%c", block[j]);
         }
+
+        free(block);
         fclose(f_out);
     }
 
