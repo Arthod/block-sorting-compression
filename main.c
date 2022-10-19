3f10b6c4f5d2123430586d1f7a7a3573b8620f7a
Ahmad Othman <32404077+Arthod@users.noreply.github.com>
2022-10-19 02:31:11 +0200

    run length test

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include "compress.c"
 
-#define BLOCK_SIZE_MAX 200000000//2000000000//500000000 * 4
+#define BLOCK_SIZE_MAX 2000000000//2000000000//500000000 * 4
                     // 2147483647
 
 #define min(a,b) (((a)<(b))?(a):(b))
@@ -13,9 +13,8 @@
 int runs_count(char *arr, int arr_length) {
     int count = 0;
     for (int i = 1; i < arr_length; i++) {
-        if (arr[i] != arr[i - 1]) {
+        if (arr[i] != arr[i - 1])
             count++;
-        }
     }
     
     return count;
@@ -23,8 +22,6 @@ int runs_count(char *arr, int arr_length) {
 
 
 int main(int argc, char** argv) {
-    printf("compressor\n");
-
     if (argc < 2) {
         printf("include file to compress as secondary argument\n");
     }
@@ -40,25 +37,33 @@ int main(int argc, char** argv) {
     int blocks_count = ceil(f_size / BLOCK_SIZE_MAX);
     for (int i = 0; i < blocks_count; i++) {
         int block_size = min(BLOCK_SIZE_MAX, f_size - BLOCK_SIZE_MAX * i);
-        char *block = malloc(block_size * sizeof(char));
-        fgets(block, block_size + 1, f);
+        uint8_t *block = malloc(block_size * sizeof(uint8_t));
+
+        for (int j = 0; j < block_size; j++) {
+            block[j] = fgetc(f);
+        }
+        //fgets(block, block_size + 1, f);
+        /*for (int j = 0; j < block_size; j++) {
+            printf("%c", block[j]);
+        }*/
 
         int runs = runs_count(block, block_size);
-        printf("Run length before %d\n", runs);
-        printf("Compressing block %d of %d with block_size=%d\n", i + 1, blocks_count, block_size);
+        printf("Runs count before %d with average run length %f\n", runs, block_size / (float) runs);
 
+        printf("Compressing block %d of %d with block_size=%d\n", i + 1, blocks_count, block_size);
+        
         int64_t bwt_primary_index = bwt_transform(block, block_size);
         runs = runs_count(block, block_size);
-        printf("Run length after %d\n", runs);
+        printf("Runs count after %d with average run length %f\n", runs, block_size / (float) runs);
 
         // Write transformed to out
         FILE *f_out = fopen("out.txt", "w");
         for (int j = 0; j < block_size; j++) {
             fprintf(f_out, "%c", block[j]);
         }
+        fclose(f_out);
 
         free(block);
-        fclose(f_out);
     }
 
     fclose(f);
