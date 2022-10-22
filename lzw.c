43986375ba409bd2a272af7d47da4fcab32505e7
Ahmad Othman <32404077+Arthod@users.noreply.github.com>
2022-10-22 03:15:44 +0200

    lzw

--- lzw.c
+++ lzw.c
@@ -30,6 +30,9 @@ void lzw_encode(uint16_t *block, uint64_t block_size) {
         dictionary[i] = *code;
     }*/
     
+    uint16_t block_new = malloc(block_size * sizeof(uint16_t));
+    uint64_t block_size_new = 0;
+
     for (uint64_t i = 0; i < block_size; i++) {
         uint64_t index_start = i;
         uint64_t index_end = i + 1;
@@ -53,12 +56,26 @@ void lzw_encode(uint16_t *block, uint64_t block_size) {
                 // Check if current index_start to index_end is in dictionary
                 int is_equal = 1;
 
-                if (is_equal)
-                for (uint64_t k = 0; k <= index_end - index_start; k++) {
-                    if (block[i + k] != block[code.index_start])
+                // Check if have same length
+                if (index_end - index_start == code.index_end - code.index_start) {
 
+                    // Check that each symbol matches
+                    for (uint64_t k = 0; k <= index_end - index_start; k++) {
+                        if (block[i + k] != block[code.index_start + k]) {
+                            is_equal = 0;
+                        }
+                    }
+                } else {
+                    is_equal = 0;
+                }
+
+                if (is_equal == 1) {
+                    break;
                 }
             }
+
+            // If symbols were found in the dictionary, we have to increment index end
+            index_end++;
         }
     }
 }
